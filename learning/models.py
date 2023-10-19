389a327a6941288683d53a6d1e4efe0037c02495
Maxim Shurygin <maximys2142@mail.ru>
2023-10-19 13:43:11 +0400

    Initial commit

---
+++ learning/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Название')
+    image = models.ImageField(upload_to='course_images/', verbose_name='Превью', blank=True, null=True)
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='course_images/', verbose_name='Превью', blank=True, null=True)
+    video_url = models.URLField(verbose_name='Ссылка на видео')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
---
+++ learning/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from learning.apps import LearningConfig
+from rest_framework.routers import DefaultRouter
+
+from learning.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
+    LessonUpdateAPIView, LessonDestroyAPIView
+
+app_name = LearningConfig.name
+
+router = DefaultRouter()
+router.register(r'course', CourseViewSet, basename='course')
+
+urlpatterns = [
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
+    path('lesson/', LessonListAPIView.as_view(), name='lesson-list'),
+    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-get'),
+    path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson-update'),
+    path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson-delete'),
+              ] + router.urls
