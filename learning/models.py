7508efb14ae17f46207c069d1651ae8c933368db
Maxim Shurygin <maximys2142@mail.ru>
2023-10-19 23:00:08 +0400

    added connection between lesson and course

--- learning/models.py
+++ learning/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Course(models.Model):
     title = models.CharField(max_length=150, verbose_name='Название')
-    image = models.ImageField(upload_to='course_images/', verbose_name='Превью', blank=True, null=True)
+    image = models.ImageField(upload_to='course_images/', verbose_name='Превью',
+                              blank=True, null=True)
     description = models.TextField(verbose_name='Описание')
 
     def __str__(self):
@@ -17,8 +18,11 @@ class Course(models.Model):
 class Lesson(models.Model):
     title = models.CharField(max_length=150, verbose_name='Название')
     description = models.TextField(verbose_name='Описание')
-    image = models.ImageField(upload_to='lesson_images/', verbose_name='Превью', blank=True, null=True)
+    image = models.ImageField(upload_to='lesson_images/', verbose_name='Превью',
+                              blank=True, null=True)
     video_url = models.URLField(verbose_name='Ссылка на видео')
+    course = models.ForeignKey(Course, related_name='lessons', on_delete=models.CASCADE,
+                               verbose_name='Курс', blank=True, null=True)
 
     def __str__(self):
         return f'{self.title}'
